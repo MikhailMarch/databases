1edd8327ae583e61bb1c9bd3b9ce7c90c186692f
MikhailMarch <63850348+MikhailMarch@users.noreply.github.com>
2021-03-09 10:19:49 +0300

    Update ex1.sql

--- week8/ex1.sql
+++ week8/ex1.sql
@@ -1,1 +1,10 @@
-
+CREATE OR REPLACE FUNCTION get_address()
+RETURNS TABLE (address VARCHAR(50)) AS
+$$
+BEGIN
+RETURN QUERY
+SELECT address.address
+FROM address
+WHERE address.city_id <= 600 and address.city_id >=400 and (address.address like '11%' or address.address like '%11%');
+END; $$
+LANGUAGE plpgsql;
