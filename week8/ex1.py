44f9cf4f13303f8fad38d8a230c06cfbce8dadaf
MikhailMarch <63850348+MikhailMarch@users.noreply.github.com>
2021-03-09 10:20:14 +0300

    Create ex1.py

---
+++ week8/ex1.py
@@ -0,0 +1,24 @@
+import psycopg2
+import geopy
+from geopy.geocoders import Nominatim
+
+con = psycopg2.connect(database="dvdrental", user="postgres",
+                       password="F143e5g6h7", host="127.0.0.1", port="5432")
+
+print("Database opened successfully")
+cur = con.cursor()
+cur.execute("SELECT * FROM get_address();")
+results = cur.fetchall()
+print("Querry processed successfully")
+cur.execute("ALTER TABLE address ADD latitude varchar(50);")
+cur.execute("ALTER TABLE address ADD longitude varchar(50);")
+for result in results:
+    print(result[0])
+    geolocator = Nominatim(user_agent="database")
+    location = geolocator.geocode(result[0])
+    if location is not None:
+        print((location.latitude, location.longitude))
+        cur.execute("UPDATE address SET latitude = %s, longitude = %s WHERE address.address = %s;",(location.latitude,location.longitude,result[0]))
+    else:
+        print(0,0)
+        cur.execute("UPDATE address SET latitude = %s, longitude = %s WHERE address.address = %s;",(0, 0, result[0]))
