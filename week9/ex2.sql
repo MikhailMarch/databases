e9bf8e6fec7d2bc4c11f887be1ecd46622e22b27
MikhailMarch <63850348+MikhailMarch@users.noreply.github.com>
2021-03-23 11:51:42 +0300

    Update ex2.sql

--- week9/ex2.sql
+++ week9/ex2.sql
@@ -7,3 +7,24 @@ create table Ledger(
 	transactionDateTime timestamp
 );
 
+begin;
+
+update account set credit = credit - 500 where account.account_id = 1;
+update account set credit = credit + 500 where account.account_id = 3;
+insert into Ledger values(1,1,3,0,500,now());
+
+update account set credit = credit - 700 where account.account_id = 2;
+update account set credit = credit + 700 where account.account_id = 1;
+update account set credit = credit - 30 where account.account_id = 2;
+update account set credit = credit + 30 where account.account_id = 4;
+insert into Ledger values(2,2,1,30,700,now());
+
+update account set credit = credit - 100 where account.account_id = 2;
+update account set credit = credit + 100 where account.account_id = 3;
+update account set credit = credit - 30 where account.account_id = 2;
+update account set credit = credit + 30 where account.account_id = 4;
+insert into Ledger values(3,2,3,30,100,now());
+
+select a.credit from account a;
+
+rollback;
