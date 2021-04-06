af41af55c53773727890eb3fb774d0756eef96ad
MikhailMarch <63850348+MikhailMarch@users.noreply.github.com>
2021-04-06 12:10:24 +0300

    Update ex3.sql

--- weel11/ex3.sql
+++ weel11/ex3.sql
@@ -1,1 +1,1 @@
-Error occurs for Serializable isolation level as it is the most strict one
+Error occurs for Serializable isolation level as it is the most strict one. It is not possible to acces the same sell within 2 transactions
