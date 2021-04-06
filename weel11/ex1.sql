e5036344c88356a4cac335e294f0fbafbfb404a7
MikhailMarch <63850348+MikhailMarch@users.noreply.github.com>
2021-04-06 11:51:44 +0300

    Update ex1.sql

--- weel11/ex1.sql
+++ weel11/ex1.sql
@@ -11,3 +11,6 @@ insert into account values('mike', 'Michael Dole', 73, 2);
 insert into account values('alyssa', 'Alyssa P. Hacker', 79, 3);
 insert into account values('bbrown', 'Bob Brown', 100, 3);
 
+for the first 2 steps terminals shows different information as we have not commited the transaction and have select statement insade it.
+When i started transaction on the second terminal and updated balance, then updated from the first terminal it stucked as there was uncommited transaction.
+After I rollbacked second one, the first terminal was able to complete. So, to prevent the lose of isolation level the first terminal update was blocked
