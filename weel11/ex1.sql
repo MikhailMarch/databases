1451249aa44d46d6a0fced5a9df7ae8658e41228
MikhailMarch <63850348+MikhailMarch@users.noreply.github.com>
2021-04-06 11:36:13 +0300

    Create ex1.sql

---
+++ weel11/ex1.sql
@@ -0,0 +1,13 @@
+CREATE TABLE account(
+username varchar(255),
+fullname varchar(255),
+balance integer,
+group_id integer
+);
+
+insert into account values('jones', 'Alice Jones', 82, 1);
+insert into account values('bitdiddl', 'Ben Bitdiddle', 65, 1);
+insert into account values('mike', 'Michael Dole', 73, 2);
+insert into account values('alyssa', 'Alyssa P. Hacker', 79, 3);
+insert into account values('bbrown', 'Bob Brown', 100, 3);
+
